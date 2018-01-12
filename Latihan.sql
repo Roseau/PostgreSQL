8a5423b846e817599f34bf91ceb445dd46752d85
Roseau <asboyo@gmail.com>
2018-01-12 16:36:47 +0700

    menambahkan Latihan 2

--- Latihan.sql
+++ Latihan.sql
@@ -134,3 +134,13 @@ where
 	salary >= (select max(salary) from employees where job_id='IT_PROG')
 order by
 	salary desc;
+--Latihan 3
+--no 1
+select * from countries where country_id = 'ID';
+
+--no 2
+INSERT INTO locations VALUES (6232, 'Cinunuk','40526','Kab. Bandung','Jawa Barat','ID'),
+							 (6231, 'Ujung Berung','40521','Kota Bandung','Jawa Barat','ID'),
+                             (6233, 'Margaharayu Raya','40525','Kota Bandung','Jawa Barat','ID'),
+                             (6230, 'Blok M','40620','Jakarta Selatan','DKI Jakarta','ID'),
+                             (6220, 'Slipi','40521','Jakarta Utara','DKI Jakarta','ID');
